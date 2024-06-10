72554316050e9fa238a226db580471564a187c9a
Linus <ray.diodone@gmail.com>
2024-06-10 18:23:23 +0200

    fast alle abfragen fertig

--- abfragen.sql
+++ abfragen.sql
@@ -4,16 +4,13 @@ from event join bewertung on event.event_id = bewertung.event_id
 group by event.titel
 order by avg(bewertung.bewertung) desc;
 
---2
-
-
---3 Anzahl und % der noch verfügbaren tickets
+--2 Anzahl und % der noch verfügbaren tickets
 select  e.titel, round(((count(*)/(l.anzahl_stehplaetze+l.anzahl_sitzplaetze))*100), 4) as "Tickets verkauft %", (l.anzahl_stehplaetze+l.anzahl_sitzplaetze - count(*)) as "Tickets Verfügbar"
 from ticket t join event e on t.event_id=e.event_id
     join location l on l.location_id=e.location_id
 group by  l.anzahl_stehplaetze, l.anzahl_sitzplaetze, e.titel;
 
---4 Welche Mitarbeiter sind an einem bestimmten Tag (15.07.2023) verfügbar
+--3 Welche Mitarbeiter sind an einem bestimmten Tag (15.07.2023) verfügbar
 select nachname as "Name"
 from mitarbeiter
 where mitarbeiter_id not IN (
@@ -22,14 +19,58 @@ where mitarbeiter_id not IN (
     where e.datum = '15-JUL-23' and mitarbeiter_id is NOT NULL
 );
 
---5 Location mit mindestens 200 Stehplätzen, 700 Sitzplätzen und Barrierefrei
+--4 Location mit mindestens 200 Stehplätzen, 700 Sitzplätzen und Barrierefrei?
 select l.bezeichnung as "Location Name", p.nachname as "Kontaktperson", p.telefonnummer as "Telefonnummer"
 from location l join person p on l.person_id=p.person_id
-where l.anzahl_sitzplätze > 700 
-    and l.anzahl_stehplätze > 200 
+where l.anzahl_sitzplaetze > 700 
+    and l.anzahl_stehplaetze > 200 
     and l.barrierefrei = 'true';
 
---10 Günstige Tickets (unter 20€)
+--5 Günstige Tickets (unter 20€)
 select distinct e.titel as "Titel", t.preis as "Preis"
 from ticket t join event e on t.event_id = e.event_id
 where preis<20;
+
+--6 Welche Mitarbeiter können an einem Tag den Einsatz übernehmen?
+select nachname
+from mitarbeiter
+where qualifikation in (
+    select a.qualifikation
+    from arbeitsschritt a join technik t on a.technik_id=t.technik_id
+        join ressource r on r.technik_id=t.technik_id
+        join einsatz e on e.ressource_id=r.ressource_id
+    where e.datum = '10-APR-24' and r.mitarbeiter_id is null
+);
+
+--7 Welcher Mitarbeiter hat die meisten Einsätze
+select m.nachname as "Name", count(*) as "Anzahl Einsätze"
+from mitarbeiter m join ressource r on m.mitarbeiter_id = r.mitarbeiter_id
+    join einsatz e on r.ressource_id = e.ressource_id
+group by m.mitarbeiter_id, m.nachname
+order by count(*)desc , m.nachname asc;
+
+--8 Welche Band hatte die meisten Zuhörer
+select b.name as "Band", count(*) as "Anzahl Zuhörer"
+from band b join event_band eb on b.band_id = eb.band_id
+    join event e on e.event_id = eb.event_id
+    join ticket t on t.event_id = e.event_id
+group by b.name
+order by count(*) desc
+fetch first row only;
+
+--9 Welcher Kunde hat die meisten Buchungen
+select nachname as "Name"
+from kunde
+where kunde_id IN (
+    select kunde_id
+    from buchung
+    group by kunde_id
+    order by count(*) desc
+    fetch first row only
+);
+
+--10 
+
+
+
+
